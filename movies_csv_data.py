e683cbc721c4e67cddb6bad782a1c7dc455a6526
unknown <blossom_ghuntla@yahoo.com>
2025-03-05 12:36:19 +0530

    fix spellings of genre

--- movies_csv_data.py
+++ movies_csv_data.py
@@ -1,5 +1,9 @@
 """Modules to open CSV"""
+
 import csv
+
+# pylint: disable=import-error
+from utils import fix_wrong_spellings
 
 # Storing all the CSV columns data in the lists.
 film_name = []
@@ -22,3 +26,20 @@ with open("movies.csv", "r", encoding="UTF-8") as m:
         rotten_tomatoes.append(data["Rotten Tomatoes %"])
         worldwide.append(data["Worldwide Gross"][1:])
         year.append(data["Year"])
+
+# Fix spelling mistakes
+# print(Counter(genre))
+# Counter({'Comedy': 41, 'Romance': 13, 'Drama': 13, 'Animation': 4,
+# 'Fantasy': 1, 'Romence': 1, 'Comdy': 1,
+# 'Action': 1, 'romance': 1, 'comedy': 1})
+
+# pylint: disable=invalid-name
+comedy_pattern = "comedy|Comdy"
+fix_wrong_spellings.fix_spellings(
+    pattern=comedy_pattern, column_category=genre, new_name="Comedy"
+)
+# pylint: disable=invalid-name
+romance_pattern = "romance|Romence"
+fix_wrong_spellings.fix_spellings(
+    pattern=romance_pattern, column_category=genre, new_name="Romance"
+)
